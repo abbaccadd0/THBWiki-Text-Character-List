eef07d3f100fe082d872d220aea80b863ef01ec3
abbaccadd0 <abbaccadd0@qq.com>
2023-04-19 21:12:19 +0800

    update

--- ClassicalTHBCharaList.user.js
+++ ClassicalTHBCharaList.user.js
@@ -5,7 +5,7 @@
 // @description:zh-CN   经典THBWiki角色列表，但或许……也有点新东西
 // @home-url            https://github.com/abbaccadd0/Classical-THBWiki-Character-List
 // @namespace           abbaccadd0
-// @version             230419.1305
+// @version             230419.2112
 // @author              abbaccadd0
 // @license             MIT License
 // @run-at              document-end
