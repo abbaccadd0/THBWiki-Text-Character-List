1fa15fbe67a3417a298c8f98609e0fe952bf89d5
abbaccadd0 <abbaccadd0@qq.com>
2024-08-18 13:13:05 +0800

    update

--- thbTextCharacterList.user.js
+++ thbTextCharacterList.user.js
@@ -5,7 +5,7 @@
 // @description:zh-CN   经典THBWiki角色列表，但或许……也有点新东西
 // @home-url            https://github.com/abbaccadd0/THBWiki-Text-Character-List
 // @namespace           abbaccadd0
-// @version             240811.1312
+// @version             240818.1313
 // @author              abbaccadd0
 // @license             MIT License
 // @run-at              document-end
